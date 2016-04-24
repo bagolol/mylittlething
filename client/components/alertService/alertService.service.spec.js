e759f6501124952d1faa80a88c4cdc9f55668bc2
Rocco <roccorighi@hotmail.com>
2016-04-24 11:10:04 +0100

    add AlertService and its unit tests

--- client/components/alertService/alertService.service.spec.js
+++ client/components/alertService/alertService.service.spec.js
@@ -1,18 +1,40 @@
 'use strict';
 
-describe('Service: alertService', function () {
+fdescribe('Service: AlertService', function () {
 
   // load the service's module
   beforeEach(module('telebuddiesApp'));
 
   // instantiate service
-  var alertService;
-  beforeEach(inject(function (_alertService_) {
-    alertService = _alertService_;
+  var AlertService;
+  beforeEach(inject(function (_AlertService_) {
+    AlertService = _AlertService_;
   }));
 
-  it('should do something', function () {
-    expect(!!alertService).toBe(true);
+  it('should default to no alert to display', function () {
+    expect(AlertService.hasAlert()).toBe(false);
   });
 
+  it('should set a success message', function(){
+    var success = { show: true, msg: 'success'};
+    AlertService.setSuccess(success);
+    expect(AlertService.getSuccess().msg).toBe('success');
+  });
+
+  it('should set an error message', function(){
+    var error = { show: true, msg: 'error'};
+    AlertService.setError(error);
+    expect(AlertService.getError().msg).toBe('error');
+  });
+
+  it('should clear error and success messages once displayed', function(){
+    var error = { show: true, msg: 'error'};
+    var success = { show: true, msg: 'success'};
+    AlertService.setSuccess(success);
+    AlertService.setError(error);
+
+    expect(AlertService.hasAlert()).toBe(true);
+    AlertService.reset();
+    expect(AlertService.hasAlert()).toBe(false);
+  });
 });
